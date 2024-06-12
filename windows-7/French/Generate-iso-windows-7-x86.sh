2dc8b49e086e471bc6841bcc854c4a8504c1e276
amidevous <amidevous@users.noreply.github.com>
2024-06-12 19:58:25 +0200

    Update and rename windows-7/Generate-iso-FR-x86--windows-7.sh to windows-7/French/Generate-iso-windows-7-x86.sh

--- windows-7/French/Generate-iso-windows-7-x86.sh
+++ windows-7/French/Generate-iso-windows-7-x86.sh
@@ -13,27 +13,27 @@ export WINEARCH=win32 WINEPREFIX=$HOME/.dism
 rm -rf $HOME/.dism
 #sudo wiminfo fr_windows_7_professional_with_sp1_x86_dvd_u_677092/sources/install.wim
 rm -f adksetup.exe
-rm -f dism-offline.part1.exe
-rm -f dism-offline.part2.rar
-rm -f dism-offline.part3.rar
-rm -f dism-offline.part4.rar
-rm -f dism-offline.part5.rar
+rm -f adkoffline.part1.exe
+rm -f adkoffline.part2.rar
+rm -f adkoffline.part3.rar
+rm -f adkoffline.part4.rar
+rm -f adkoffline.part5.rar
 #original use with adksetup /quiet /layout c:\adkoffline https://download.microsoft.com/download/9/A/E/9AE69DD5-BA93-44E0-864E-180F5E700AB4/adk/adksetup.exe
-wget https://github.com/amidevous/windowspostinstall/releases/download/0.0.1/dism-offline.part1.exe
-wget https://github.com/amidevous/windowspostinstall/releases/download/0.0.1/dism-offline.part2.rar
-wget https://github.com/amidevous/windowspostinstall/releases/download/0.0.1/dism-offline.part3.rar
-wget https://github.com/amidevous/windowspostinstall/releases/download/0.0.1/dism-offline.part4.rar
-wget https://github.com/amidevous/windowspostinstall/releases/download/0.0.1/dism-offline.part5.rar
+wget https://github.com/amidevous/windowspostinstall/releases/download/0.0.1/adkoffline.part1.exe
+wget https://github.com/amidevous/windowspostinstall/releases/download/0.0.1/adkoffline.part2.rar
+wget https://github.com/amidevous/windowspostinstall/releases/download/0.0.1/adkoffline.part3.rar
+wget https://github.com/amidevous/windowspostinstall/releases/download/0.0.1/adkoffline.part4.rar
+wget https://github.com/amidevous/windowspostinstall/releases/download/0.0.1/adkoffline.part5.rar
 sudo winetricks --self-update
 winetricks -q dotnet452 wininet
 winetricks win10
 #auto use adksetup.exe /features + /q
-wine dism-offline.part1.exe
-rm -f dism-offline.part1.exe
-rm -f dism-offline.part2.rar
-rm -f dism-offline.part3.rar
-rm -f dism-offline.part4.rar
-rm -f dism-offline.part5.rar
+wine adkoffline.part1.exe
+rm -f adkoffline.part1.exe
+rm -f adkoffline.part2.rar
+rm -f adkoffline.part3.rar
+rm -f adkoffline.part4.rar
+rm -f adkoffline.part5.rar
 #mkdir -p /home/andy/.dism/bin/
 #cp -R "/home/andy/.dism/drive_c/Program Files/Windows Kits/10/Assessment and Deployment Kit/Deployment Tools/x86/DISM/"* /home/$USER/.dism/bin/
 #wine "Z:\home\%USERNAME%\.dism\bin\dism.exe" /Get-ImageInfo /imagefile:"Z:\home\$USER\fr_windows_7_professional_with_sp1_x86_dvd_u_677092\sources\install.wim"
