bb38aa4c41e2046f23207bb654b89ec150b621e8
amidevous <amidevous@users.noreply.github.com>
2024-06-12 20:53:59 +0200

    Update Generate-iso-windows-7-x86.sh

--- windows-7/French/Generate-iso-windows-7-x86.sh
+++ windows-7/French/Generate-iso-windows-7-x86.sh
@@ -9,26 +9,30 @@ sudo mkdir -p /mnt
 sudo mount fr_windows_7_professional_with_sp1_x86_dvd_u_677092.iso /mnt
 sudo cp -av /mnt/. fr_windows_7_professional_with_sp1_x86_dvd_u_677092
 sudo umount /mnt
+sudo chown -R $USER:$USER fr_windows_7_professional_with_sp1_x86_dvd_u_677092/
+sudo chown -R $USER:$USER fr_windows_7_professional_with_sp1_x86_dvd_u_677092/*
+sudo chown -R 777 fr_windows_7_professional_with_sp1_x86_dvd_u_677092/
+sudo chown -R 777 fr_windows_7_professional_with_sp1_x86_dvd_u_677092/*
 export WINEARCH=win32 WINEPREFIX=$HOME/.dism
-rm -rf $HOME/.dism
+sudo rm -rf $HOME/.dism
 #sudo wiminfo fr_windows_7_professional_with_sp1_x86_dvd_u_677092/sources/install.wim
-rm -f adksetup.exe
-rm -f adkoffline.part1.exe
-rm -f adkoffline.part2.rar
-rm -f adkoffline.part3.rar
-rm -f adkoffline.part4.rar
-rm -f adkoffline.part5.rar
+sudo rm -f adksetup.exe
+sudo rm -f adkoffline.part1.exe
+sudo rm -f adkoffline.part2.rar
+sudo rm -f adkoffline.part3.rar
+sudo rm -f adkoffline.part4.rar
+sudo rm -f adkoffline.part5.rar
 #original use with adksetup /quiet /layout c:\adkoffline https://download.microsoft.com/download/9/A/E/9AE69DD5-BA93-44E0-864E-180F5E700AB4/adk/adksetup.exe
 wget https://github.com/amidevous/windowspostinstall/releases/download/0.0.1/adkoffline.part1.exe
 wget https://github.com/amidevous/windowspostinstall/releases/download/0.0.1/adkoffline.part2.rar
 wget https://github.com/amidevous/windowspostinstall/releases/download/0.0.1/adkoffline.part3.rar
 wget https://github.com/amidevous/windowspostinstall/releases/download/0.0.1/adkoffline.part4.rar
 wget https://github.com/amidevous/windowspostinstall/releases/download/0.0.1/adkoffline.part5.rar
-sudo winetricks --self-update
-winetricks -q dotnet452 wininet
-winetricks win10
+sudo WINEARCH=win32 WINEPREFIX=$HOME/.dism winetricks --self-update
+WINEARCH=win32 WINEPREFIX=$HOME/.dism winetricks -q dotnet452 wininet
+WINEARCH=win32 WINEPREFIX=$HOME/.dism winecfg -v win10
 #auto use adksetup.exe /features + /q
-wine adkoffline.part1.exe
+WINEARCH=win32 WINEPREFIX=$HOME/.dism wine adkoffline.part1.exe
 rm -f adkoffline.part1.exe
 rm -f adkoffline.part2.rar
 rm -f adkoffline.part3.rar
@@ -37,6 +41,7 @@ rm -f adkoffline.part5.rar
 #mkdir -p /home/andy/.dism/bin/
 #cp -R "/home/andy/.dism/drive_c/Program Files/Windows Kits/10/Assessment and Deployment Kit/Deployment Tools/x86/DISM/"* /home/$USER/.dism/bin/
 #wine "Z:\home\%USERNAME%\.dism\bin\dism.exe" /Get-ImageInfo /imagefile:"Z:\home\$USER\fr_windows_7_professional_with_sp1_x86_dvd_u_677092\sources\install.wim"
+WINEARCH=win32 WINEPREFIX=$HOME/.dism wine "C:\Program Files\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\x86\DISM\dism.exe" /Get-ImageInfo /imagefile:"Z:\home\$USER\fr_windows_7_professional_with_sp1_x86_dvd_u_677092\sources\install.wim"
 #dism /mount-image /imagefile:"C:\Users\%USERNAME%\Downloads\Windowsextractiso\sources\install.wim" /index:1 /mountdir:c:\Mount
 #Dism /Image:C:\mount /Add-Package /PackagePath:"C:\Users\%USERNAME%\Downloads\windows10.0-kb4016635-x64_2b1b48aa6ec51c019187f15059b768b1638a21ab.msu"
 #dism /unmount-image /mountdir:c:\mount /commit
